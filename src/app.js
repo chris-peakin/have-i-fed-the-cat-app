f5975ac72ce1091f6371ce40fb1844782cb04ca7
Chris Peakin <christopher.peakin@googlemail.com>
2022-02-26 11:39:06 +0000

    added gets and posts

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
+const {Cat} = require('./models');
 app.use(express.json());
 
-app.post('/', (req, res) => {
-    return res.status(201).json({result: req.body});
-})
+app.post('/cats', (req, res) => {
+    Cat.create(req.body).then(cat => res.status(201).json(cat));
+});
+
+app.get('cats', (req, res) => {
+    Cat.findAll(req.body).then(cat => res.status(201).json(cat));
+});
+
+app.post('/cats/:catId', (req, res) => {
+    Cat.findAll(req.body).then(cat => res.status(201).json(cat));
+});
+
+app.post('/cats/:catId', (req, res) => {
+    Cat.update(req.body, {where: {id: req.params.catId}});
+});
 
 module.exports = app;
