a77b6fc0b0e0879c75b540ead144a64f69c7ab0c
Chris Peakin <christopher.peakin@googlemail.com>
2022-02-26 11:08:58 +0000

    sequelize added

--- src/app.js
+++ src/app.js
@@ -2,4 +2,8 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+app.post('/', (req, res) => {
+    return res.status(201).json({result: req.body});
+})
+
 module.exports = app;
